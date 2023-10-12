d28a9901113c746bde0e16e24f5c6dde235bc979
mohamed meqlad <76888049+mohamedmm9@users.noreply.github.com>
2023-10-12 14:54:28 +0300

    Add files via upload

---
+++ stream_to_kafka_dag.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+from stream_to_kafka import start_streaming
+
+start_date = datetime(2018, 12, 21, 12, 12)
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': start_date,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+
+with DAG('random_people_names', default_args=default_args, schedule_interval='0 1 * * *', catchup=False) as dag:
+
+
+    data_stream_task = PythonOperator(
+    task_id='kafka_data_stream',
+    python_callable=start_streaming,
+    dag=dag,
+    )
+
+    data_stream_task
